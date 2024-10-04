df46a0e48230ac7aabd296899c665e2ad005c76d
BenDev181 <benjiro181@gmail.com>
2024-10-04 16:25:32 +0100

    ScooterApp class and tests so far

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -1,7 +1,72 @@
-// require the User and Scooter classes - see where they can be used in ScooterApp.js
+const User = require('./User.js')
+const Scooter = require('./Scooter.js')
 
 class ScooterApp {
-  // ScooterApp code here
+  constructor() {
+    this.stations = {
+      'London': [],
+      'Manchester': [],
+      'Edinburgh': []
+    }
+    this.registeredUsers = {};
+  }
+  registerUser(username, password, age) {
+    if (this.registeredUsers[username]) {
+      throw new Error('already registered')
+    }
+    if (age <= 18) {
+      throw new Error('too young to register')
+    }
+    const newUser = new User(username, password, age)
+    this.registeredUsers[username] = newUser
+    console.log('user has been registered')
+    return newUser
+  }
+  loginUser(username, password) {
+    const user = this.registeredUsers[username]
+    if (!user) { //Also ensures password is a match
+      throw new Error('User or password is invalid')
+    }
+    user.login(password)
+    console.log('user has been logged in')
+  }
+  logoutUser() {
+
+  }
+  createScooter(station) {
+    if (!this.stations.hasOwnProperty(station)) {
+      throw new Error('no such station')
+    }
+    const newScooter = new Scooter(station)
+    console.log('created new scooter')
+    this.stations[station].push(newScooter)
+    newScooter.station = station
+    return newScooter
+  }
+  dockScooter(scooter, station) {
+    if (!this.stations.hasOwnProperty(station)) {
+      throw new Error('no such station')
+    }
+    if (false/*this.stations[station] == scooter*/) {
+      throw new Error('scooter already at station')
+    }
+
+    scooter.dock(station)
+    this.stations[station].push(scooter)
+    console.log('scooter is docked')
+  }
+  rentScooter(scooter, user) {
+    scooter.rent(user)
+  }
+  print() {
+    console.log(this.stations)
+  }
 }
+
+const scooterApp = new ScooterApp()
+const newScooter = new Scooter("Manchester")
+scooterApp.dockScooter(newScooter, "Manchester")
+console.log(scooterApp.stations)
+scooterApp.print()
 
 module.exports = ScooterApp
--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -1,14 +1,45 @@
-const User = require('../src/User')
-const ScooterApp = require('../src/ScooterApp')
+const ScooterApp = require('../src/ScooterApp.js')
+const User = require('../src/User.js')
+const Scooter = require('../src/Scooter.js')
 
-const scooterApp = new ScooterApp()
-// ScooterApp tests here
+let scooterApp;
+beforeEach(() => {
+  scooterApp = new ScooterApp()
+})
 
-// register user
-describe('registerUser method tests', () => {
-  test('Should return instance of User', () => {
+describe('ScooterApp Property Tests', () => {
+  test('ScooterApp class should create ScooterApp instance', () => {
+    expect(scooterApp).toBeInstanceOf(ScooterApp)
+  })
+})
+
+describe('ScooterApp Method Tests', () => {
+  test('Should return instance of User when registerUser() is called', () => {
     const response = scooterApp.registerUser('Joe Bloggs', 'test123', 21)
     expect(response).toBeInstanceOf(User)
+  })
+  test('LoginUser() should login user', () => {
+    scooterApp.registerUser('Joe Bloggs', 'test123', 21)
+    scooterApp.loginUser('Joe Bloggs', 'test123')
+    expect(user.loggedIn).toBe(true)
+  })
+  test('LogoutUser() logs out user', () => {
+    scooterApp.loginUser('Joe Bloggs', 'test123')
+    scooterApp.logoutUser('Joe Bloggs')
+    expect(user.loggedIn).toBe(false)
+  })
+  test('CreateScooter() creates a new scooter, adds it to station scooter list', () => {
+    scooterApp.createScooter('Manchester')
+    expect(scooterApp.stations['Manchester'].length).toBe(1)
+    expect(() => {scooterApp.createScooter('Man')
+    }).toThrow('no such station')
+  })
+  test('DockScooter() adds scooter to dock list and returns error if not possible', () => {
+    const newScooter = new Scooter('Manchester')
+    scooterApp.dockScooter(newScooter, 'London')
+    expect(scooterApp.stations['London'].length).toBe(1)
+    expect(() => {scooterApp.dockScooter('Man')
+    }).toThrow('no such station')
   })
 })
 
