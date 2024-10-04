91a15ca9cd36090450574685911f413a67fbe36d
BenDev181 <benjiro181@gmail.com>
2024-10-04 11:28:45 +0100

    Scooter class and it's tests

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,36 @@
+const User = require('./User.js')
+
 class Scooter {
-  // scooter code here
+  static nextSerial = 1
+  constructor(station) {
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial
+    Scooter.nextSerial++
+    this.charge = 100
+    this.isBroken = false
+  }
+  rent(user) {
+    if (!(user instanceof User)) {
+      throw new Error('User not recognised')
+    }
+    if (this.charge <= 20) {
+      throw new Error('scooter needs to charge')
+    }
+    if (this.isBroken == true) {
+      throw new Error('scooter needs repair')
+    }
+    this.user = user
+    this.station = null
+  }
+  dock(station) {
+    this.station = station
+    this.user = null
+  }
 }
+
+//const scooter1 = new Scooter("London")
+//console.log(scooter1)
+
 
 module.exports = Scooter
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,20 +1,48 @@
 const Scooter = require('../src/Scooter')
 
+let scooter1;
+
+beforeEach(() => {
+  Scooter.nextSerial = 1
+  scooter1 = new Scooter("London")
+  scooter2 = new Scooter("London")
+})
+
 // typeof scooter === object
 describe('scooter object', () => {
   test('Scooter class should create Scooter instance', () => {
-    const scooter = new Scooter()
-    expect(scooter).toBeInstanceOf(Scooter)
+    expect(scooter1).toBeInstanceOf(Scooter)
+  })
+  test('Scooter class correctly assigns station property', () => {
+    expect(scooter1.station).toBe("London")
+  })
+  test('Scooter class initially assigns user to null', () => {
+    expect(scooter1.user).toBe(null)
+  })
+  test('Scooter class initially assigns serial as 1 and every additional scooter sequentially from 1', () => {
+    expect(scooter1.serial).toBe(1)
+    expect(scooter2.serial).toBe(scooter1.serial + 1)
+  })
+  test('Scooter class initially assigns charge to 100', () => {
+    expect(scooter1.charge).toBe(100)
+  })
+  test('Scooter class initially assigns isBroken to false', () => {
+    expect(scooter1.isBroken).toBe(false)
   })
 })
 
 // Method tests
 describe('scooter methods', () => {
-  // tests here!
-
-  // rent method
-
-  // dock method
+  test('Scooter is checked out if physical conditions are met', () => {
+    scooter1.rent("Ben")
+    expect(scooter1.user).toBe("Ben")
+    expect(scooter1.station).toBe(null)
+  })
+  test('Scooter is returned to station when dock(station) is called', () => {
+    scooter1.dock("Manchester")
+    expect(scooter1.station).toBe("Manchester")
+    expect(scooter1.user).toBe(null)
+  })
 
   // requestRepair method
 
